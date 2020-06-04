ca189c3f19a105d9c42bcc38ea19c1cc2589b494
Diablo <info@m.demoneum.com>
2020-06-04 22:26:08 +0200

    Icons and text can be used in navigationitems now at the same time.

--- lib/nav/NavigationItem/index.tsx
+++ lib/nav/NavigationItem/index.tsx
@@ -7,38 +7,59 @@ interface Props {
   name?: string
   url?: string
   icon?: any
+  iconSize?: number
   index?: boolean
   className?: string
   onClick?: () => void
 }
 
-export default class NavigationItem extends React.Component<Props, {}> {
-  render(): JSX.Element {
-    const { url, onClick, name, icon } = this.props
+const NavigationItem: React.FC<Props> = props => {
+  const { url, onClick, name, icon, iconSize } = props
 
-    return (
-      <li>
-        {url && this.renderLink()}
-        {!url && !icon && <a onClick={onClick}>{name}</a>}
-        {!url && icon && <a onClick={onClick}><SVG html={icon} size={{ width: 16, height: 16 }} /></a>}
-      </li>
-    )
-  }
-
-  renderLink(): JSX.Element {
-    const { name, url, index, icon, className } = this.props
-
-    return icon ? (
-      <Link href={url!}>
-        <a className={className}>
-          {!name && icon && <SVG html={icon} size={{ width: 16, height: 16 }} />}
+  return (
+    <li>
+      {url && <NavigationItemLink {...props} />}
+      {!url && !icon && <a onClick={onClick}>{name}</a>}
+      {!url && icon && (
+        <a onClick={onClick}>
+          <SVG
+            html={icon}
+            size={{ width: iconSize || 16, height: iconSize || 16 }}
+          />
         </a>
-      </Link>
-    ) : (
-        <Link href={url!}>
-          <a className={className}>{name}</a>
-        </Link>
-      )
-  }
+      )}
+    </li>
+  )
 }
 
+const NavigationItemLink: React.FC<Props> = props => {
+  const { name, url, index, icon, iconSize, className } = props
+
+  return icon ? (
+    <Link href={url!}>
+      <a className={className}>
+        {!name && icon && (
+          <SVG
+            html={icon}
+            size={{ width: iconSize || 16, height: iconSize || 16 }}
+          />
+        )}
+        {name && icon && (
+          <span>
+            <SVG
+              html={icon}
+              size={{ width: iconSize || 16, height: iconSize || 16 }}
+            />
+            <p>{name}</p>
+          </span>
+        )}
+      </a>
+    </Link>
+  ) : (
+    <Link href={url!}>
+      <a className={className}>{name}</a>
+    </Link>
+  )
+}
+
+export default NavigationItem
